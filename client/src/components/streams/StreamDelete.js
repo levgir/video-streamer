f4a6bb8eebfdfb078b22d670520994cbc5cf9cce
Levi Giraud <levigiraud@Levis-MacBook-Air.local>
2020-04-09 20:31:14 -0600

    set up a modal to display when a user wants to delete a stream, using portal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return <div>Stream Delete</div>;
+  const actions = (
+    <>
+      <div className="ui button negative">Delete</div>
+      <div className="ui button">Cancel</div>
+    </>
+  );
+
+  return (
+    <div>
+      Stream Delete
+      <Modal
+        title="Delete Your Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
